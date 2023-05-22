7c78d46166b4da945fc4efac5346e8fc202230fc
pdro-dev <104535409+pdro-dev@users.noreply.github.com>
2023-05-22 23:12:40 +0000

    ..

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const fs = require('fs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.get('/car-list', (req, res) => {
+  fs.readFile('data/car-list.json', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading file');
+    } else {
+      const cars = JSON.parse(data);
+      res.json(cars);
+    }
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+
+
